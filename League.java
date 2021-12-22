3f39bacb953b5f1e9a54d5631ea32a6357e29232
Tich <tzvidzie@gmail.com>
2021-12-23 01:01:26 +0200

    string format

--- League.java
+++ League.java
@@ -15,29 +15,102 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.lang.model.util.ElementScanner14;
+
 
 class League {
 
     
     public static void main(String[] args) throws IOException {
         
-        BufferedReader buf; 
+       /* BufferedReader buf; 
+       
         try { 
             buf = new BufferedReader(new FileReader( args[0]));
-            String game;
-            while ((game = buf.readLine()) != null) {
-              // System.out.println(game + "  :");
-               AddResult(game);
-            }
-            buf.close();
-           
-        } 
-        
-        catch(Exception e)
+
+        }  catch(Exception e)
         {
         System.out.println( String.format(e.toString() + " \n Oops!! Please include a valid filename.\n " +
                                         "For example => java League input.txt"));      
         }
+   */
+
+        Map<String, Integer> table = new HashMap<>();
+        BufferedReader buf = new BufferedReader(new FileReader( args[0]));
+            String game;
+            while ((game = buf.readLine()) != null) {
+              
+               String[] res = game.split(",", 2);  //{ Lions 3, Sankes 3}
+
+
+     String [] teamA = res[0].split(" ", 2);
+         String [] teamB = res[1].split(" ", 2);
+         
+         System.out.println(( res[0].split(" ", 2)[0] ));
+         System.out.println(( res[1].split(" ", 2)[0] ));
+         System.out.println(( res[0].split(" ", 2)[1] ));
+         System.out.println(( res[1].split(" ", 2)[1] ));
+
+
+        //int t1 = Integer.parseInt( res[0].split(" ", 2)[1] );
+       // int t2  =  Integer.parseInt( res[1].split(" ", 2)[1] );
+
+    
+  /*  if (  t1 > t2)
+   { 
+       if(!table.containsKey(teamA[0])) 
+       {
+        table.put(teamA[0],3);
+       }
+       else if (t2 > t1)
+           table.put(teamB[0],3);
+        else 
+        { 
+            table.put(teamA[0],3);
+            table.put(teamB[0],3);
+        }
+       
+      //  p.put(key, map.getKey()+1);
+       }  
+
+*/
+
+
+
+            /*
+
+                   Lions 3, Snakes 3
+                   Tarantulas 1, FC Awesome 0
+                    Lions 1, FC Awesome 1
+                   Tarantulas 3, Snakes 1
+                   Lions 4, Grouches 0
+
+            */
+
+
+
+
+       
+               // storing the values
+               table.put("Juventus FC", 1);
+               table.put("Bayern Munich FC", 80);
+               table.put("Norwich City FC", 38);
+               table.put("Manchester United", 100);
+               table.put("Chelsea FC", 78);
+               printLeagueTable(table);
+
+
+
+
+
+
+
+            }
+            buf.close();
+           
+        
+        
+      
         //finally{ }
  
        
@@ -48,19 +121,7 @@ class League {
 
     private static void AddResult(String game) {
 
-        String[] res = game.split(",", 2);  //{ Lions 3, Sankes 3}
-        String teamA = res[0];
-        String teamB = res[1];
-
-        Map<String, Integer> table = new HashMap<>();
-
-		// storing the values
-        table.put("Juventus FC", 1);
-        table.put("Bayern Munich FC", 80);
-		table.put("Norwich City FC", 38);
-		table.put("Manchester United", 100);
-		table.put("Chelsea FC", 78);
-        printLeagueTable(table);
+      
     }
 
     private static void printLeagueTable(Map<String, Integer> table) {
