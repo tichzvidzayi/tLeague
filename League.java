0dfda97f05f9cf73fc3ef9353d95bba0a4711afe
Tich <tzvidzie@gmail.com>
2021-12-22 23:24:57 +0200

    Adding

--- League.java
+++ League.java
@@ -3,16 +3,31 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.http.HttpHeaders;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 class League {
+
+    
     public static void main(String[] args) throws IOException {
         
         BufferedReader buf; 
         try { 
             buf = new BufferedReader(new FileReader( args[0]));
-            String line;
-            while ((line = buf.readLine()) != null) {
-               System.out.println(line + "  :");
+            String game;
+            while ((game = buf.readLine()) != null) {
+              // System.out.println(game + "  :");
+               AddResult(game);
             }
             buf.close();
            
@@ -25,7 +40,53 @@ class League {
         }
         //finally{ }
  
-         
+       
+
+
         
     }
+
+    private static void AddResult(String game) {
+
+        String[] res = game.split(",", 2);  //{ Lions 3, Sankes 3}
+        String teamA = res[0];
+        String teamB = res[1];
+
+        Map<String, Integer> namesAges = new HashMap<>();
+
+		// storing the values
+        namesAges.put("Juventus FC", 1);
+        namesAges.put("Bayern Munich FC", 80);
+		namesAges.put("Norwich City FC", 38);
+		namesAges.put("Manchester United", 100);
+		namesAges.put("Chelsea FC", 78);
+        printLeagueTable(namesAges);
+    }
+
+    private static void printLeagueTable(Map<String, Integer> namesAges) {
+    
+    	Map<String, Integer> ht = namesAges.entrySet()
+			.stream()
+			.sorted(Collections.reverseOrder(Entry.comparingByValue()))
+			.collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(),
+					(entry1, entry2) -> entry2, LinkedHashMap::new));
+         int position =1;
+		for (Map.Entry<String, Integer> entry : ht.entrySet()) {
+			System.out.println(position++ + ". " + entry.getKey() + ", " +
+                                           entry.getValue().toString() + 
+                                       ( ( entry.getValue() == 1)? " pt" : " pts"));
+		}
+    
+
+
+        /*
+        1. Tarantulas, 6 pts
+2. Lions, 5 pts
+3. FC Awesome, 1 pt
+3. Snakes, 1 pt
+5. Grouches, 0 pts
+
+        */
+    }
+
 }
