a1d7d9efe6efc87c39d5c4570c9d0d1ab47e8938
Tich <tzvidzie@gmail.com>
2021-12-22 13:50:27 +0200

    read-file

--- League.java
+++ League.java
@@ -1,5 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 class League {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! \n Merry Christmas folks!!");
+    public static void main(String[] args) throws IOException {
+        
+
+        BufferedReader br = new BufferedReader(new FileReader( args[0]));
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+               System.out.println(line + "  :");
+            }
+        } finally {
+            br.close();
+        }
     }
 }
